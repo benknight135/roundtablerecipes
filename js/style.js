82f5dfb1849467aaf492d6e6b7ba9d5ac6b2f6d4
Ben <benknight135@gmail.com>
2020-07-04 16:49:36 +0100

    fix link to stylesheet

--- js/style.js
+++ js/style.js
@@ -1,4 +1,4 @@
 document.write(`
 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-<link rel="stylesheet" type="text/css" href="css/style.css"></link>
+<link rel="stylesheet" type="text/css" href="/css/style.css"></link>
 `);
