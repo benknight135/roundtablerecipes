da00493ce41f9163e1b1a1ab7805c17f32d15240
Ben <benknight135@gmail.com>
2020-07-04 17:01:12 +0100

    add favicon

--- js/style.js
+++ js/style.js
@@ -1,4 +1,5 @@
 document.write(`
 <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 <link rel="stylesheet" type="text/css" href="/css/style.css"></link>
+<link rel="icon" href="/logo/rtr_logo.png" type="image/png" sizes="16x16">
 `);
