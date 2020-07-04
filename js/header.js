b1f03d03943e8401d2f683384d9e843a4512a25b
Ben <benknight135@gmail.com>
2020-07-04 16:35:10 +0100

    Latest recipe link to header

--- js/header.js
+++ js/header.js
@@ -1,3 +1,4 @@
 document.write(`
 <button onclick="location.href='/home.html'" type="button">Round Table Recipes</button>
+<button onclick="location.href='/recipes/chorizoomelette/chorizoomelette.html'" type="button">Latest Recipe</button>
 `);
