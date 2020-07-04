e86abf111c3c84ef1f2bc4e10272fe4717be9430
Ben <benknight135@gmail.com>
2020-07-04 16:42:43 +0100

    add meta viewport

--- js/header.js
+++ js/header.js
@@ -1,4 +1,5 @@
 document.write(`
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <button onclick="location.href='/home.html'" type="button">Round Table Recipes</button>
 <button onclick="location.href='/recipes/chorizoomelette/chorizoomelette.html'" type="button">Latest Recipe</button>
 `);
